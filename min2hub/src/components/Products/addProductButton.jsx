33f33347f43cf1d76d97e7a9db06b83f52cc47db
fernandolpz21 <fernando.lpzg2@gmail.com>
2022-10-26 18:02:18 -0500

    addProductButton jsx & css

---
+++ min2hub/src/components/Products/addProductButton.jsx
@@ -0,0 +1,116 @@
+//import { useNavigate  } from "react-router-dom"
+import React, {useState} from 'react'
+import './addProduct.css'
+//import { useAlert } from 'react-alert'
+
+
+const AddProductButton = () => {
+    //const alert = useAlert();
+    /*  ---  MODAL FORM --- */
+    
+    //Product register fields
+    const [product,setProduct] = useState({
+        "name": '',
+        "description": '',
+        "image": ''
+    });
+
+    const {name, description, image} = product;
+
+    //copies product data and changes the target of the onchange
+    const onChange = e => {
+        // Actualizamos para los diferentes tipos de datos recopilados
+        setProduct({...product, [e.target.name]:e.target.value}); 
+        console.log(e.target.value)
+    }
+    const onSubmit = e => {
+        e.preventDefault();
+        console.log(product)
+        if(name === '' || description === '' || image === ''){
+            //missing info
+        }
+        else{
+            try {
+                addToDB(product);
+                close();
+                resetForm();
+                //alert.success('Producto añadido exitosamente');
+            } catch (error) {
+                //alert.error(error.response.data.msg);
+            }
+            
+        }
+    }
+    
+    /*  ---  MODAL ACTIONS --- */
+    let modal = document.querySelectorAll(".modal")[0];
+    let modalContainer = document.querySelectorAll(".modal-container")[0];
+
+    const open = e => {
+        e.preventDefault();
+        modalContainer.style.opacity = "1";
+        modalContainer.style.visibility ="visible";
+        modal.classList.toggle("modal-close");
+    }
+    const close = () => {
+        modal.classList.toggle("modal-close");
+        setTimeout(function(){
+            modalContainer.style.opacity = "0";
+            modalContainer.style.visibility = "hidden";
+        }, 600)//vamos a darle 1s para ejecutar la función
+    }
+    
+    window.addEventListener("click", function(e){
+        if(e.target === modalContainer){
+            close();
+        }
+    })
+
+    const resetForm = () => {
+        setProduct({...product, 
+            "name"  : '',
+            "description" : '',
+            "image": ''
+        });
+    }
+
+
+    /*  ---  ADD TO DATABASE --- */
+
+    const addToDB = (product) => {
+        console.log(JSON.stringify(product));
+        fetch("/addProduct", {
+          method: 'POST',
+          headers:{
+            'Content-type': 'application/json; charset=UTF-8'
+          },
+          body: JSON.stringify(product)
+        }).then(console.log("Data inserted to products in min2Hub DB"));
+    }
+
+    return(
+        <section>
+            <a href="#" class='addProductButton' onClick={open}>.</a>
+            <div class = "modal-container">
+                <div class = "modal modal-close">
+                    <a class = "close" onClick={close}>X</a>
+                    <div class = "modal-text">
+                        <h2 className='header'>Agregar nuevo producto</h2>
+                        <form id = "formulario" onSubmit={onSubmit}>
+                            <div className='grid-2'>
+                                <div>
+                                    <input placeholder='Nombre' type='text' name = 'name' value={name} onChange = {onChange} required/>
+                                    <input placeholder='Descripción' type='text' name = 'description' value={description} onChange = {onChange} required/>
+                                    <input placeholder='URL Foto' type = 'text' name = 'image' value={image} onChange = {onChange} required/>
+                                </div>
+                            </div>
+                            <input type="submit" value = "Agregar Producto" formTarget='_blank'/>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+        
+    )
+}
+export default AddProductButton
