a17468c8004ab52c2d8b73e2e1615acd41a7e82b
carloseag1234 <carloseag1234@gmail.com>
2022-10-26 09:42:38 -0500

    Menu update

--- min2hub/src/components/Menu/Menu.jsx
+++ min2hub/src/components/Menu/Menu.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import "./Menu.css";
 
 function Menu() {
     return (
       <div className="Menu">
-        
+        <head>
+          <title>Menu Desplegable</title>
+        </head>
+        <body>
+          <div id="header">
+            <img src="" alt="" srcset="" />
+            <ul class="nav">
+              <li><a href="">Pedidos</a></li>
+              <li><a href="">Productos</a></li>
+              <li><a href="">Inicio</a></li>
+              <li><a href="">Perfil</a></li>
+            </ul>
+          </div>
+        </body>
       </div>
     );
   }
