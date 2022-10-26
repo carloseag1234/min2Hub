9c340feb5396f319a716d7e12347a109f733a8cc
fernandolpz21 <fernando.lpzg2@gmail.com>
2022-10-26 09:44:13 -0500

    mySQL database connection

--- server/index.js
+++ server/index.js
@@ -5,3 +5,26 @@ const app = express();
 app.listen(PORT, () => {
  console.log(`Server listening on ${PORT}`);
 });
+
+
+// Conexión con el servidor
+var mysql = require('mysql'); // Para usar el módulo de mysql y controlar la base de datos
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "nodered",
+    password: "0000",
+    database: "nodejs"
+});
+
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Database connected successfully!");
+});
+  
+  
+//404 handeler
+app.get("/", (req, res) => {
+    res.status(404).send("404 not found");
+});
