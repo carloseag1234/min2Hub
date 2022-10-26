a966b87cf40912f1691d484c303c12f60ee55de2
A01641937-alf <88739339+A01641937-alf@users.noreply.github.com>
2022-10-26 10:11:33 -0500

    Footer

--- min2hub/src/components/Footer/Footer.jsx
+++ min2hub/src/components/Footer/Footer.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-
+import img from "./img/logo.png"
 
 
 function Footer() {
     return (
       <div className="Footer">
           <div className="Logo">
-            <img src={"https://www.google.com/imgres?imgurl=https%3A%2F%2Fw7.pngwing.com%2Fpngs%2F391%2F829%2Fpng-transparent-diwan-bookstore-logo-publishing-book-angle-text-reading.png&imgrefurl=https%3A%2F%2Fwww.pngwing.com%2Fes%2Fsearch%3Fq%3Dlogos&tbnid=czTke_hM66KFoM&vet=12ahUKEwjBtcHShP76AhVHgyYFHRPrBwQQMygCegUIARDpAQ..i&docid=wIbZFMs5DRzetM&w=920&h=512&q=logo%20en%20png&ved=2ahUKEwjBtcHShP76AhVHgyYFHRPrBwQQMygCegUIARDpAQ"}/>
+            <img src={img}/>
           </div>
           <div className="Text">
             <text >
