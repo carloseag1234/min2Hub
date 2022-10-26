09551164ba0194f79360349977008cfea1d73e96
MarianaEsquivelHernandez <esquivelmariana0702@gmail.com>
2022-10-26 09:39:01 -0500

    test for Description

--- min2hub/src/App.js
+++ min2hub/src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
-import { Footer } from './components/Footer';
-import { Menu } from './components/Menu';
-import { Home } from './components/Home';
+//import { Footer } from './components/Footer';
+//import { Menu } from './components/Menu';
+//import { Home } from './components/Home';
 import { TarjetaInd } from './components/TarjetaInd';
-import { Products } from './components/Products';
-import { Pedidos } from './components/Pedidos';
+//import { Products } from './components/Products';
+//import { Pedidos } from './components/Pedidos';
 
 function App() {
   return (
     <div className="App">
-      
+      <TarjetaInd />
     </div>
   );
 }
--- min2hub/src/components/TarjetaInd/TarjetaInd.jsx
+++ min2hub/src/components/TarjetaInd/TarjetaInd.jsx
@@ -3,9 +3,12 @@ import React from "react";
 function TarjetaInd() {
     return (
       <div className="TarjetaInd">
-        
+        <div class="popup" onclick="myFunction()">Click me!
+          <span class="popuptext" id="myPopup">Popup text...</span>
+        </div>
       </div>
     );
   }
   
   export default TarjetaInd;
+
