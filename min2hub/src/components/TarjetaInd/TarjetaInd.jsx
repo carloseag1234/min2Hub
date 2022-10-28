c8cfeed2490b7c60d06cf25ceca8887701f705ec
MarianaEsquivelHernandez <esquivelmariana0702@gmail.com>
2022-10-27 22:55:57 -0500

    Im running out of time

--- min2hub/src/components/TarjetaInd/TarjetaInd.jsx
+++ min2hub/src/components/TarjetaInd/TarjetaInd.jsx
@@ -1,15 +1,42 @@
-import React from "react"
-import './TarjetaInd.css'
+import React from "react";
+import './TarjetaInd.css';
 
 function TarjetaInd() {
     return (
       <div className="TarjetaInd">
-        <div class="popup" onclick="myFunction()">Click me!
-          <span class="popuptext" id="myPopup">Popup text...</span>
-        </div>
+          <section class="popupCard">
+            <section class="leftSide">
+                <h1>Nombre</h1>
+                <section class="productInfo">
+                    <section>
+                        <h3>Usuario:</h3>
+                        <p>peres@gmail.com</p>
+                    </section>
+                    <section>
+                        <h3>Tipo de Producto:</h3>
+                        <p>Fruta</p>
+                    </section>
+                    <section>
+                        <h3>Lugar:</h3>
+                        <p>Aviación</p>
+                    </section>
+                </section>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae nostrum ipsum delectus, voluptatibus, itaque dolore debitis cum accusantium voluptate cupiditate earum asperiores quibusdam porro ullam illo modi ipsam provident beatae.</p>
+                <section class="progressBar">
+                    <section class="currentProgress"></section>
+                </section>
+                <section class="bottomPart">
+                    <button class="shopingCartButton">Agregar al carro</button>
+                    <button class="descriptionButton"><span></span></button>
+                </section>
+            </section>
+            <section class="rightSide">
+                <button><span></span></button>
+                <img src="caja de comida.png" alt="foto del producto"></img>
+            </section>
+          </section>
       </div>
     );
   }
   
   export default TarjetaInd;
-
