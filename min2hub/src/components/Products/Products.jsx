c0a330967f821e81fc935d9739f1d041e9e8c1e9
Juan Salazar <jjsc2003@live.com>
2022-10-27 20:37:44 -0500

    Caja de comidaW

--- min2hub/src/components/Products/Products.jsx
+++ min2hub/src/components/Products/Products.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import './estilo.css';
+import cajaComida from '../../img/cajaDeComida.png';
 
 function Products() {
     return (
       <div className="Products">
         <section>
-          <img src="caja de comida.png"></img>
+          <img src={cajaComida}></img>
           <h3>Caja de viveres</h3>
           <p>Esto es una prueba de lo Que sería un párrafo con una pequeña información……</p>
           <section class="progressBar">
